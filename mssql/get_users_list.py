9a213b6daaf92bf76607e8d694e1fadb7f579afd
“Santu <roysantu2002@yahoo.com>
2022-11-05 20:39:29 +0530

    added crypto and rsa

--- mssql/get_users_list.py
+++ mssql/get_users_list.py
@@ -6,11 +6,19 @@ username = 'SA'
 password = 'Ansible1234'
 # ENCRYPT defaults to yes starting in ODBC Driver 18. It's good to always specify ENCRYPT=yes on the client side to avoid MITM attacks.
 cnxn = mssql_connection(server, database, username, password)
-cursor = cnxn.mssql_ccursor()
+
+conn = cnxn.mssql_connector()
+cursor = conn.cursor()
+
 
 #Sample select query
-cursor.execute("SELECT @@version;")
-row = cursor.fetchone()
-while row:
-    print(row[0])
-    row = cursor.fetchone()
+# cursor.execute("SELECT @@version;")
+# cursor.execute("select loginname from master.sys.syslogins;")
+# row = cursor.fetchone()
+# while row:
+#     print(row[0])
+#     row = cursor.fetchone()
+
+_user_sql = "select loginname from master.sys.syslogins;"
+user_list = cnxn.get_users(_user_sql)
+print(user_list)
--- mssql/mssql_connector.py
+++ mssql/mssql_connector.py
@@ -1,10 +1,20 @@
+import hashlib
 import logging as log
 import os
+import sys
 
 import pyodbc
 
-
+log.basicConfig(
+    level=log.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        log.FileHandler("debug.log"),
+        log.StreamHandler(sys.stdout)
+    ]
+)
 class mssql_connection:
+
     """mssql db connection"""
     def __init__(self, server, database, username, passw):
         self.server = server
@@ -13,11 +23,11 @@ class mssql_connection:
         self.passw = passw
         try:
             log.debug("[%s] Opening connection to database", self.server)
+            log.info("[%s] connecting ...", self.server)
             self.connector = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+self.server+';DATABASE='+self.database+';ENCRYPT=yes;UID='+self.username+';PWD='+ self.passw +';TrustServerCertificate=yes')
-            print("{c} is working".format(c=self.connector))
+            log.info("[%s] connected", self.server)
 
         except pyodbc.Error as e:
-            print(self.server, str(e))
             log.error("[%s] Failed to connect to database: %s", self.server, str(e))
             self.connector.close()
 
@@ -27,15 +37,36 @@ class mssql_connection:
     def mssql_connector(self) -> str:
         if self.connector:
             return self.connector
+        else:
+            return None
 
     """
         #TODO:
         mssql_ccursor
     """
-    def mssql_ccursor(self) -> str:
+    def cursor(self):
         if self.connector:
             return self.connector.cursor()
+        else:
+            return None
 
-    # def get_users(self) -> dict:
+    """
+        #TODO:
+        mssql_ccursor
+    """
+    def get_users(self, sql: str) -> list:
+        cursor = self.cursor()
+        cursor.execute(sql)
+        users_list = []
+        users_ = [item[0] for item in cursor.fetchall()]
+        # users_ = cursor.fetchall()
+        if len(users_)==0:
+            return None
+        else:
+            for user in users_:
+                user_id_sha256 = hashlib.sha256(user.encode())
+                users_list.append(user_id_sha256.hexdigest())
+            return users_list
+
 
 
